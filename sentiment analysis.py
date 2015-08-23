8f2fca1b469247623e5f949b0bdcb73503d10f8a
Amey Ruikar <ruikar@usc.edu>
2015-08-23 13:50:13 -0700

    SVC linear

--- sentiment analysis.py
+++ sentiment analysis.py
@@ -2,8 +2,12 @@
 
 import math
 import csv
+from sklearn.preprocessing import scale
+from sklearn.linear_model import *
 from sklearn import svm
 import random
+
+
 text = open("happy.txt","r")
 for line in text:
     words=line.split()
@@ -18,7 +22,7 @@ for word in words:
     try:
         happy_vector.append(int(happy_map[word]))    
     except KeyError:
-        print("notfound ",word)
+        'print("notfound ",word)'
 
 
 
@@ -29,7 +33,7 @@ print("started")
 for i in range(len(happy_map)):
         happy_feature.append(0)
     
-for j in range(500):
+for j in range(50000):
     
     for i in range(len(happy_map)):
         happy_feature[i]=0
@@ -42,14 +46,14 @@ for j in range(500):
 
 
 y5=[]
-for j in range(500):
+for j in range(50000):
     y5.append(5)
 
 
 b=[]
 
 print("500 done")
-for j in range(300):
+for j in range(50000):
     for i in range(len(happy_map)):
         happy_feature[i]=0
 
@@ -60,7 +64,7 @@ for j in range(300):
     a.append(happy_feature)
 
 
-for j in range(300):
+for j in range(50000):
     y5.append(3)
 
 
@@ -68,7 +72,7 @@ c=[]
 
 print("300 done")
 
-for j in range(200):
+for j in range(50000):
     for i in range(len(happy_map)):
         happy_feature[i]=0
 
@@ -77,19 +81,37 @@ for j in range(200):
     a.append(happy_feature)
 
 
-for j in range(200):
+for j in range(50000):
     y5.append(1)
     
-
-clf = svm.SVC()
+'''
+a = scale(a)
+clf = svm.SVC(kernel = 'linear')
 clf.fit(a, y5)
+
+logit = LogisticRegression(C=1.0)
+
+logit.fit(a,y5)
+'''
 
 text = []
 for i in range(len(happy_map)):
         text.append(0)
         
-for k in range(0):
-        text[int(random.randrange(1,len(text)))]=1
+for k in range(3):
+        text[random.randrange(1,len(text))]=1
 
+'''
+print(clf.decision_function(text))
+print(clf.score(text, [5], None))
+print(clf.score(text, [3], None))
+print(clf.score(text, [1], None))
 print(clf.predict(text))
 
+
+print "predicted:", logit.predict(text)
+print "decision: ",logit.decision_function(a)
+print "probability",logit.predict_proba(text)
+
+print "params: ",logit.get_params(deep=True)
+'''
